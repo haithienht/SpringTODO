a088573380dbb0ec05b7348b4af53009e1758242
Hai Thien <39566043+haithienht@users.noreply.github.com>
2020-05-16 21:51:50 +0700

    update login function with validation
    TODO: add authencation with Spring Security

--- src/main/java/com/npht/springtodo/controller/client/HomeController.java
+++ src/main/java/com/npht/springtodo/controller/client/HomeController.java
@@ -1,6 +1,7 @@
 package com.npht.springtodo.controller.client;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import com.npht.springtodo.dto.UserLogin;
@@ -8,15 +9,10 @@ import com.npht.springtodo.model.User;
 import com.npht.springtodo.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BeanPropertyBindingResult;
-// import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,6 +23,9 @@ public class HomeController {
 
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    private HttpSession session;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String getHome(Model model) {
@@ -55,6 +54,16 @@ public class HomeController {
 
         // return "post login";
         // response.setStatus(HttpServletResponse.SC_OK);
+        //TODO: Add authentication with Spring Security, JDBC "spring boot database authentication session"
+        if (!errors.hasErrors()) {
+            User u = userRepo.findByEmailAndPassword(user.getEmail(), user.getPassword());
+            if (u == null) {
+                errors.reject("common", "Invalid login information. Please check again your email and password.");
+            } else {
+                u.setPassword("");
+                session.setAttribute("loggedInUser", u);
+            }
+        }
         return errors.getAllErrors();
     }
 
--- src/main/java/com/npht/springtodo/repository/UserRepository.java
+++ src/main/java/com/npht/springtodo/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.npht.springtodo.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 import com.npht.springtodo.model.User;
 
 public interface UserRepository extends CrudRepository<User,Long> {
-
+    User findByEmailAndPassword(String email, String password);
 }
